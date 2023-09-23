4e11c9418b90c84b6b7e5d22c515a033a61c00b6
huzeyfe <huzeyfe.for.is@gmail.com>
2023-09-23 15:28:10 +0300

    first commit

--- variables.py
+++ variables.py
@@ -1,6 +1,50 @@
 maasAli = 5000
 maasAhmed = 4000
-vergi = 0,27
+vergi = 0.27
 
 print (maasAli - (maasAli*vergi))
 print (maasAhmed - (maasAhmed*vergi))
+
+# Değişken tanımlama kuralları:
+# rakam ile başlayamaz
+# ona değer atmalıyız
+# aynı değişken ismini iki kez kullanımamalı
+
+number1 = 10
+print(number1)
+
+number1 = 20
+print(number1)
+
+number1 += 30
+print(number1)
+
+# Büyük küçük harf duyarlılığı
+
+age = 12
+AGE = 100
+
+print("age: " + str(age))
+print("AGE: " + str(AGE))
+
+# Türkçe karakter kullanmayalım
+# Boşluk burakılmaz
+
+yas = 20
+_age = 20
+
+x = 1              # int
+y = 2.3            # float 
+name = "Çınar"     # string
+isStudent = True   # bool
+
+# x, y, name, isStudent = (1, 2.3, "Çınar", True)
+
+a = '10'
+b = '20'
+print(a+b) # => 1020
+
+firstName = "Huzeyfe"
+lastName = " Muhammed"
+
+print(firstName + lastName)  # Huzeyfe Muhammed
---
+++ variablesDeneme.py
@@ -0,0 +1,19 @@
+customerFirstName = "Ali"
+customerLastName = "Kaşmer"
+customerName = customerFirstName + customerLastName
+customerGender = "Erkek"
+customerTC = "65*******23"
+customerBirthDay = 1997
+customeAddres = ""
+customerAge = 2023 -   customerBirthDay
+
+print (customerAge)
+
+
+order1 = 110
+order2 = 1100.5
+order3 = 356.95
+
+totalOrder = order1 + order2 + order3
+
+print  ("Toplam: " , totalOrder)
