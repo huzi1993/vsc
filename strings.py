5ce2210de185e976466a09257e4ac93a39810be9
huzeyfe <huzeyfe.for.is@gmail.com>
2023-09-26 19:30:17 +0300

    second commit

---
+++ strings.py
@@ -0,0 +1,19 @@
+name = "Huzeyfe"
+surname = "Muhammed"
+age = 30
+
+# print ("My name is" , name , surname , "and \nI am" , age , "years old.")
+
+greeting = "My name is " + name + " " + surname + " and \nI am " + str (age) + " years old"
+length = len (greeting)
+
+# print (greeting)
+# print (greeting[0])
+# print (greeting[3])
+# print (len (greeting))
+# print(greeting[length-1])
+# print(greeting[-1])
+# print(greeting[3:7])
+# print(greeting[3:])
+# print(greeting[:18])
+print(greeting[2:40:3])
