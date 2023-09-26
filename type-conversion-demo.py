a3bb47b099d61b864f7e064fda37adaea2d5d3c4
huzeyfe <huzeyfe.for.is@gmail.com>
2023-09-26 18:23:40 +0300

    second commit

---
+++ type-conversion-demo.py
@@ -0,0 +1,9 @@
+pi= 3.14
+
+yariCap = float (input ("Dairenin yarı çapı giriniz: "))
+
+daireninAlani = pi * yariCap  ** 2
+daireninCevresi = 2 * pi * yariCap
+
+print ("Dairenin alanı: ", " ", daireninAlani)
+print ("Dairenin çevresi: ", " ", daireninCevresi)
