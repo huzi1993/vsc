75bbbf2167d5e18f3a06d449fd5d9e53c65101dc
huzeyfe <huzeyfe.for.is@gmail.com>
2023-10-05 02:58:41 +0300

    second commit

---
+++ format.py
@@ -0,0 +1,18 @@
+name = "Huzeyfe"
+surname = "Muhammed"
+age = 30
+
+# print ("My name is {} {}.".format(name,surname))
+# print ("My name is {0} {1}.".format(name,surname))
+# print ("My name is {s} {n}.".format(n=name,s=surname))
+# print ("My name is {} {} and I'm {} years old.".format(name,surname, age))
+# print ("My name is {} {} and I'm {} years old.".format(name,surname, 30))
+print (f"My name is {name} {surname} and I'm {age} years old.")
+
+
+# result =  200 / 700
+
+# print ("the result is: {}".format(result))
+# print ("the result is: {r:1.3}".format(r=result))
+
+
--- strings-demo.py
+++ strings-demo.py
@@ -3,38 +3,47 @@ course  = "Python Kursu: Baştan Sona Python Programlama Rehberiniz (40 saat)"
 
 # 1- 'course' karakter dizisinde kaç karakter bulunmaktadır ?
 result = len(course)
-length = len(website)
+length = len (website)
 
 # 2- 'website' içinden www karakterlerini alın.
-result = website[7:10]
+result = website [7:10]
+
 
 # 3- 'website' içinden com karakterlerini alın.
-result = website[22:25]
-result = website[length-3:length]
+
+result = website [22:25]
+result = website [length-3:length]
+
+
 
 # 4- 'course' içinden ilk 15 ve son 15 karakterlerini alın.
-result = course[0:15]
-result = course[:15]
-result = course[-15:]
-
+result = course [:15]
+lenCourse = len(course)
+result = course [0:15] + " - " + course [lenCourse-15:lenCourse]
+result = course [0:15] + " - " + course [-15:]
 # 5- 'course' ifadesindeki karakterleri tersten yazdırın.
-result = course[::-1]
 
-name, surname, age, job = 'Huzeyfe','Muhammed', 30, 'Tercüman' 
+result = course [::-1]
+name, surname, age, job = "Huzeyfe","Muhammed", 30, "Tercüman" 
 
 # 6- Yukarıda verilen değişkenler ile ekrana aşağıdaki ifadeyi yazdırın.
-#    'Benim adım Huzeyfe Muhammed, Yaşım 32 ve mesleğim Tercüman.' 
+#    "Benim adım Huzeyfe Muhammed, Yaşım 32 ve mesleğim Tercüman." 
 
-result = "Benim adım "+ name+ " " + surname+ ", Yaşım "+ str(age) + " ve mesleğim "+ job
-result = "Benim adım {0} {1}, Yaşım {2} ve mesleğim {3}.".format(name,surname,age,job) 
-result = f'Benim adım {name} {surname}, Yaşım {age} ve "mesleğim" {job}.'
+result = f"Benim adım {name} {surname}, yaşım {age} ve mesleğim {job}"
+result = "Benim adım " + name  + " " + surname + ", yaşım " + str(age) + " ve mesleğim " +job
+result = "Benim adım {} {}, yaşım {} ve mesleğim {}".format (name, surname, age, job)
 
-# 7- 'Hello world' ifadesindeki w harfini 'W' ile değiştirin.
-s = 'Hello world'
-s = s[0:6] + 'W'+ s[-4:]
 
-print(s)
-# 8- 'abc' ifadesini yan yana 3 defa yazdırın.
-result = 'abc ' * 3
+# 7- "Hello world" ifadesindeki w harfini "W" ile değiştirin.
 
-print(result)
+
+stringim = "Hello world"
+stringim = stringim[0:6] + "W" + stringim[-4:]
+stringim.replace("w","W")
+# print (stringim)
+
+# 8- "abc" ifadesini yan yana 3 defa yazdırın.
+abc = "abc "
+result = abc * 3
+
+print (result)
